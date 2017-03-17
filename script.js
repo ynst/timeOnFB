8216e47b0a660913d24c50feba417ef193740364
yunustuncbilek <yunus.tuncbilek@yale.edu>
2017-03-16 22:37:10 -0400

    Parse Username. Multiple Tabs bug fixed

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 chrome.extension.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if (request.action == 'load') {
-			var time = 1 + parseInt(localStorage['seconds']);
+			var time = parseInt(localStorage['seconds']);
 			var date = localStorage['date'];
 			sendResponse({secounds: time, date: date});
 		}
 		if( request.action == 'save') {
 			localStorage['seconds'] = request.secounds;
 			localStorage['date'] = request.date;
+			localStorage['userName'] = request.userName;
 		}
 	}
 );
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-38323138-1']);
-_gaq.push(['_trackPageview']);
+// var _gaq = _gaq || [];
+// _gaq.push(['_setAccount', 'UA-38323138-1']);
+// _gaq.push(['_trackPageview']);
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+// (function() {
+//   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+//   ga.src = 'https://ssl.google-analytics.com/ga.js';
+//   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+// })();
--- time_tracking.js
+++ time_tracking.js
@@ -3,6 +3,7 @@ var FbExtension = {
 	lastTimestamp: 0,
 	list: null,
 	link: null,
+	userName: null,
 	initiate: function() {
 		// this.list = document.createElement('li');
 		// this.list.className = "_2s25";
@@ -13,11 +14,20 @@ var FbExtension = {
 		this.link.innerHTML = 'wait..';
 		// this.list.appendChild(this.link);
 
-		var fbList;
-		var tinyman;
-		if( (fbList = document.getElementsByClassName('_1k67 _4q39')[0]) )
-			tinyman = fbList.firstChild;
-			fbList.insertBefore(this.link, tinyman);
+		// var fbList;
+		// var tinyman;
+
+		// parse username from HTML
+		if ( (nameTag = document.getElementsByClassName('_2s25')[0])){
+			this.userName = nameTag.getElementsByTagName("span")[0].innerHTML;
+		}
+
+		// inserts the time
+		if( (fbList = document.getElementsByClassName('_1k67 _4q39')[0]) ){
+			fbList.insertBefore(this.link, fbList.firstChild);
+		}
+
+		// console.log(fbList);
                         // fbList.insertBefore(this.list, tinyman);
 
 		chrome.extension.sendMessage({action: 'load'}, this.loaded.bind(this));
@@ -29,11 +39,10 @@ var FbExtension = {
 		this.totalSecounds = data.secounds || 0;
 		this.lastTimestamp = data.date || this.getTimestamp();
 
-		this.track();
-		window.setInterval(FbExtension.track.bind(FbExtension), 1000);
-		window.addEventListener('unload', FbExtension.onWindowClose.bind(FbExtension), false);
+		setInterval(this.trackTime.bind(this), 1000);
+		// window.addEventListener('unload', FbExtension.onWindowClose.bind(FbExtension), false);
 	},
-	track: function() {
+	trackTime: function() {
 		var currentTimestamp;
 
 		if( (currentTimestamp = this.getTimestamp()) != this.lastTimestamp ) {
@@ -42,9 +51,17 @@ var FbExtension = {
 
 		this.totalSecounds++;
 		this.lastTimestamp = currentTimestamp;
-		this.update();
+
+		chrome.extension.sendMessage({
+			action: 'save', 
+			secounds: this.totalSecounds, 
+			date: this.lastTimestamp, 
+			userName: this.userName
+		});
+
+		this.updateClock();
 	},
-	update: function() {
+	updateClock: function() {
 		var hours,minutes,secounds;
 
 		hours = Math.floor(this.totalSecounds / 3600);
@@ -64,9 +81,6 @@ var FbExtension = {
 			(d.getMonth() + 1 < 10 ? "0" + (d.getMonth()+1) : (d.getMonth()+1)),
 			(d.getDate() < 10 ? "0"+d.getDate() : d.getDate())
 		].join('');
-	},
-	onWindowClose: function() {
-		chrome.extension.sendMessage({action: 'save', secounds: this.totalSecounds, date: this.lastTimestamp});
 	}
 };
 FbExtension.initiate();
