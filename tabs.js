59d1050a06441d98386362b2bd39a451dead1c39
yunustuncbilek <yunus.tuncbilek@yale.edu>
2017-03-21 14:37:48 -0400

    Showing time from last day

--- tabs.js
+++ tabs.js
@@ -1,34 +1,38 @@
 var FbExtension = {
 	totalSecounds: 0,
+	yesterdayTotalSeconds: 0,
 	lastTimestamp: 0,
 	list: null,
 	link: null,
 	userName: null,
 	initiate: function() {
-		// this.list = document.createElement('li');
-		// this.list.className = "_2s25";
 		this.link = document.createElement('a');
 		this.link.className = "_2s25";
 		
 		this.link.href = "#";
 		this.link.innerHTML = 'wait..';
-		// this.list.appendChild(this.link);
-
-		// var fbList;
-		// var tinyman;
-
-		// parse username from HTML
+		
 		if ( (nameTag = document.getElementsByClassName('_2s25')[0])){
 			this.userName = nameTag.getElementsByTagName("span")[0].innerHTML;
 		}
 
-		// inserts the time
+		// inserts the time before fbList (profile picture)
 		if( (fbList = document.getElementsByClassName('_1k67 _4q39')[0]) ){
 			fbList.insertBefore(this.link, fbList.firstChild);
 		}
 
 		// console.log(fbList);
                         // fbList.insertBefore(this.list, tinyman);
+
+
+        this.yesterdayTimeDisplay = document.createElement('a');
+		this.yesterdayTimeDisplay.className = "_2s25";
+		this.yesterdayTimeDisplay.innerHTML = this.htmlTime(this.yesterdayTotalSeconds);
+
+		// inserts the time before fbList (profile picture and name)
+		if( (fbList = document.getElementsByClassName('_1k67 _4q39')[0]) ){
+			fbList.insertBefore(this.yesterdayTimeDisplay, fbList.firstChild);
+		}
 
 		chrome.extension.sendMessage({action: 'load'}, this.loaded.bind(this));
 	},
@@ -38,6 +42,7 @@ var FbExtension = {
 
 		this.totalSecounds = data.secounds || 0;
 		this.lastTimestamp = data.date || this.getTimestamp();
+		this.yesterdayTotalSeconds = data.yesterdayTotalSeconds || 0;
 		// window.addEventListener('unload', FbExtension.onWindowClose.bind(FbExtension), false);
 
 		var trackId = window.setInterval(this.trackTime.bind(this), 1000);
@@ -51,7 +56,9 @@ var FbExtension = {
 	trackTime: function() {
 		var currentTimestamp;
 
+		// day turn
 		if( (currentTimestamp = this.getTimestamp()) != this.lastTimestamp ) {
+			this.yesterdayTotalSeconds = this.totalSecounds;
 			this.totalSecounds = 0;
 		}
 
@@ -61,6 +68,7 @@ var FbExtension = {
 		chrome.extension.sendMessage({
 			action: 'save', 
 			secounds: this.totalSecounds, 
+			yesterdayTotalSeconds: this.yesterdayTotalSeconds,
 			date: this.lastTimestamp, 
 			userName: this.userName
 		});
@@ -68,17 +76,8 @@ var FbExtension = {
 		this.updateClock.bind(this)();
 	},
 	updateClock: function() {
-		var hours,minutes,secounds;
-
-		hours = Math.floor(this.totalSecounds / 3600);
-		minutes = Math.floor( (this.totalSecounds - hours*3600) / 60 );
-		secounds = this.totalSecounds - minutes*60 - hours*3600;
-
-		if( hours < 10 ) hours = "0"+hours;
-		if( minutes < 10 ) minutes = "0"+minutes;
-		if( secounds < 10 ) secounds = "0"+secounds;
-
-		this.link.innerHTML = hours+":"+minutes+":"+secounds;
+		this.link.innerHTML = this.htmlTime(this.totalSecounds);
+		this.yesterdayTimeDisplay.innerHTML = this.htmlTime(this.yesterdayTotalSeconds);
 	},
 	getTimestamp: function() {
 		var d = new Date();
@@ -87,6 +86,19 @@ var FbExtension = {
 			(d.getMonth() + 1 < 10 ? "0" + (d.getMonth()+1) : (d.getMonth()+1)),
 			(d.getDate() < 10 ? "0"+d.getDate() : d.getDate())
 		].join('');
+	},
+	htmlTime: function(seconds){
+		var hours,minutes,secounds;
+
+		hours = Math.floor(seconds / 3600);
+		minutes = Math.floor( (seconds - hours*3600) / 60 );
+		secounds = seconds - minutes*60 - hours*3600;
+
+		if( hours < 10 ) hours = "0"+hours;
+		if( minutes < 10 ) minutes = "0"+minutes;
+		if( secounds < 10 ) secounds = "0"+secounds;
+
+		return hours+":"+minutes+":"+secounds;
 	}
 };
 FbExtension.initiate()
